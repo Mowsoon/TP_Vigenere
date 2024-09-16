58f8e8e3b746ec81cbb25dbb9aff411acac842e9
Mowsoon <alban.munari@gmail.com>
2024-09-16 17:05:09 +0200

    ajout fonction calcul de pgcd et raccourcissement affiche

--- affichage.py
+++ affichage.py
@@ -1,39 +1,40 @@
+import math
+
 
 def affiche(x):
     text = input("Entrer votre {} : ".format(x))
 
-    text = text.lower()
-    text = "".join(filter(str.isalnum, text))
+    text = "".join([char for char in text.lower() if char.isalnum()])
 
     return text
-    
-def cryptage(message, codage): 
+
+def cryptage(message, codage):
 
     text = ""
     j = 0
     for i in range(len(message)):
         if j >= len(codage):
             j = 0
-        lettre = ord(message[i]) - 96 
+        lettre = ord(message[i]) - 96
         cle = ord(codage[j]) - 96
         valeur = lettre + cle
 
         if valeur > 26:
             valeur -= 26
-            
+
         text += chr(valeur + 96)
         j += 1
 
     return text
 
 def decryptage(message, codage):
-    
+
     text = ""
     j = 0
     for i in range(len(message)):
         if j >= len(codage):
             j = 0
-        lettre = ord(message[i]) - 96 
+        lettre = ord(message[i]) - 96
         cle = ord(codage[j]) - 96
         valeur = lettre - cle
         if valeur < 1:
@@ -45,10 +46,10 @@ def decryptage(message, codage):
 def occurence(texte):
     occu = []
     occurrences = {}
-    
+
     longueur = len(texte)
 
-    
+
     for i in range(longueur):
         for j in range(i + 3, longueur + 1):
             sequence = texte[i:j]
@@ -56,7 +57,7 @@ def occurence(texte):
                 occurrences[sequence] += 1
             else:
                 occurrences[sequence] = 1
-    
+
     count_max = 0
     for sequence, count in occurrences.items():
         if count > 1:
@@ -70,7 +71,7 @@ def occurence(texte):
                 occu.append(count)
     occu.append(texte)
     return occu
-    
+
 def distance(occu):
     position = []
     distance = []
@@ -84,10 +85,17 @@ def distance(occu):
 
     for i in range(len(position) - 1):
         distance.append(position[i+1] - position[i])
-    
+
     return distance
 
-'''
+
+def longueur_cle(distance):
+    pgcd = distance[0]
+    for i in range(distance[:1]):
+        pgcd = math.gcd(pgcd, i)
+    return pgcd
+
+
 texte = affiche("Message")
 cle = affiche("cle")
 crypter = cryptage(texte,cle)
@@ -95,4 +103,4 @@ decrypter = decryptage(crypter,cle)
 print("message crypter : {}\nmessage decrypter : {}".format(crypter, decrypter))
 '''
 occu = occurence("abcdefghijklmnopqrstuvwxyzabcdmnoabc")
-print(distance(occu))
+print(longueur_cle(distance(occu)))'''
